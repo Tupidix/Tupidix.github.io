f6a2a1539c1909d64c4c42dea91f1c16005eb644
Ryan Dorasamy <ryan.dorasamy@heig-vd.ch>
2024-01-17 02:22:19 +0100

    modif générale

---
+++ app.js
@@ -0,0 +1,6 @@
+// document.querySelector(`main section.active`)?.classList.remove("active");
+// 	if (hashSplited[0] == "#search") {
+// 		document.querySelector(`#search-input`).classList.add("active");
+// 	} else {
+// 		document.querySelector(`#search-input`)?.classList.remove("active");
+// 	}
