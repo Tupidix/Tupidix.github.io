cd13ec6b890d4e970e6ce5ece11eafeab46ab92d
Ryan Dorasamy <ryan.dorasamy@heig-vd.ch>
2024-01-17 15:39:40 +0100

    dark mode fonctionnel

--- app.js
+++ app.js
@@ -4,3 +4,28 @@
 // 	} else {
 // 		document.querySelector(`#search-input`)?.classList.remove("active");
 // 	}
+
+const themeSwitch = document.getElementById("theme-switch");
+const body = document.getElementById("body");
+const normal = document.getElementById("defaut");
+const special = document.getElementById("special");
+
+themeSwitch.addEventListener("click", () => {
+    if (normal.className === "active"){
+        normal.className = "inactive";
+    } else {
+        normal.className = "active";
+    }
+
+    if (special.className === "active"){
+        special.className = "inactive";
+    } else {
+        special.className = "active";
+    }
+
+	if (body.className === "light") {
+		body.className = "dark";
+	} else if (body.className === "dark") {
+		body.className = "light";
+	}
+});
